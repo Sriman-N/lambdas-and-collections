a895d048b3c198892490ea9000a7b6f24d5fed4e
Sriman-N <sriman411@gmail.com>
2022-12-24 13:40:17 -0500

    second

--- learning_lambdas.py
+++ learning_lambdas.py
@@ -11,3 +11,9 @@ domain = [1, 2, 3, 4, 5]
 
 # f(x) = x * 2
 our_range = map(lambda num: num * 2, domain)
+print(list(our_range))
+
+different_range = map(lambda a: a * 2, domain)
+print(list(different_range))
+
+print(list(map(lambda b: b * 2, domain)))
