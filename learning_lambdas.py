bfddafaa48e8cbc0b7703dcdaa4ca22487e4113f
Sriman-N <sriman411@gmail.com>
2022-12-24 13:12:24 -0500

    initial

---
+++ learning_lambdas.py
@@ -0,0 +1,13 @@
+def square(num):
+    return num * num
+
+square(2) # => 4
+
+square_lambda = lambda num: num * num
+
+assert square(4) == square_lambda(4)
+
+domain = [1, 2, 3, 4, 5]
+
+# f(x) = x * 2
+our_range = map(lambda num: num * 2, domain)
