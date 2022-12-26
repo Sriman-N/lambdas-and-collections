be5dab68d33c717612db11ba383ab4c03b27bf3d
Sriman-N <sriman411@gmail.com>
2022-12-26 17:57:00 -0500

    initial

--- collection_funcs.py
+++ collection_funcs.py
@@ -1,6 +1,11 @@
+from functools import reduce
 domain = [1, 2, 3, 4, 5]
 
+# f(x) = x * 2
 our_range = map(lambda num: num * 2, domain)
 print(list(our_range))
 
-#evens = filter(lambda num: )
+evens = filter(lambda num: num % 2 == 0, domain)
+print(list(evens))
+
+the_sum = reduce(lambda acc, num: acc + num, domain, 0)
