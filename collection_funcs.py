2c07182b64ee402de6a7154a8f5df720e4f228d3
Sriman-N <sriman411@gmail.com>
2023-01-03 12:41:57 -0500

    initial

--- collection_funcs.py
+++ collection_funcs.py
@@ -9,3 +9,14 @@ evens = filter(lambda num: num % 2 == 0, domain)
 print(list(evens))
 
 the_sum = reduce(lambda acc, num: acc + num, domain, 0)
+print(the_sum)
+
+words = ['Boss', 'a', 'Alfred', 'fig', 'Daemon', 'dig']
+print("Sorting by default")
+print(sorted(words))
+
+print("Sorting with a lambda key")
+print(sorted(words, key=lambda s: s.lower()))
+
+print("Sorting with a method")
+words.sort(key=str.lower())
