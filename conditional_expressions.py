28b42a602b81919d423e4fccf929c9c9898de920
Sriman-N <sriman411@gmail.com>
2023-02-03 21:36:52 -0500

    initial

---
+++ conditional_expressions.py
@@ -0,0 +1,21 @@
+# a) if CONDITION:
+#      my_var = 1
+#  else:
+#      my_var = 2
+
+# b) my_var = 1 if CONDITION else 2
+
+#These two above (meaning a and b) mean the same thing
+
+#print("A") if CONDITION else print("B")
+
+a = 5
+b = 6
+
+my_var = 1 if a < b else 2
+
+print(my_var)
+
+my_var = 1 if a > b else 2
+
+print(my_var)
